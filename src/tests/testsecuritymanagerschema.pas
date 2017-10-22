9595533ae0c2894165728ca47962feab83ae08b6
Andreas <andi@andifriess.com>
2017-10-22 16:13:17 +0200

    extend the tests

--- src/tests/testsecuritymanagerschema.pas
+++ src/tests/testsecuritymanagerschema.pas
@@ -9,6 +9,8 @@ uses
 
 type
 
+  { TTestSecurityManagerSchema }
+
   TTestSecurityManagerSchema= class(TTestCase)
   protected
     procedure SetUp; override;
@@ -17,11 +19,18 @@ type
     // CRUD TAuthorization(s)
     procedure CR_TAuthorization;
     // CRUD TCustomUser
+
+    // CRUD Schema
+    procedure CHK_SchemaType;
+    procedure CR_SchemaLevel;
+    procedure CR_SchemaUserAuth;
+
   end;
 
 implementation
 uses
   security.manager.schema; // DUT location
+
 
 procedure TTestSecurityManagerSchema.CR_TAuthorization;
 var
@@ -39,6 +48,47 @@ begin
     FreeAndNil(Dut);
   end;
 end;
+
+procedure TTestSecurityManagerSchema.CHK_SchemaType;
+begin
+  // Test class function
+  AssertTrue('class function TUsrMgntSchema.UsrMgntType not correct', (TUsrMgntSchema.UsrMgntType = TUsrMgntType.umtUnknown));
+  //AssertTrue('class function TUsrLevelMgntSchema.UsrMgntType not correct', (TUsrLevelMgntSchema.UsrMgntType = TUsrMgntType.umtLevel));
+  //AssertTrue('class function TAuthBasedUsrMgntSchema.UsrMgntType not correct', (TAuthBasedUsrMgntSchema.UsrMgntType = TUsrMgntType.umtUnknown));
+  //AssertTrue('class function TUsrAuthSchema.UsrMgntType not correct', (TUsrAuthSchema.UsrMgntType = TUsrMgntType.umtAuthorizationByUser));
+  //AssertTrue('class function TUsrGroupAuthSchema.UsrMgntType not correct', (TUsrGroupAuthSchema.UsrMgntType = TUsrMgntType.umtAuthorizationByUserAndGroup));
+  //AssertTrue('class function TGroupAuthSchema.UsrMgntType not correct', (TGroupAuthSchema.UsrMgntType = TUsrMgntType.umtAuthorizationByGroup));
+end;
+
+procedure TTestSecurityManagerSchema.CR_SchemaLevel;
+var
+  ASchema: TUsrMgntSchema;
+begin
+  // Now create a new one
+  ASchema:=TUsrLevelMgntSchema.Create(1, 100, 1);
+  try
+    AssertTrue('UsrMgntType not correct', (ASchema.UsrMgntType = TUsrMgntType.umtLevel) );
+    AssertTrue('TUsrLevelMgntSchema is not correct set', (ASchema is TUsrLevelMgntSchema));
+//
+//    with ASchema as TUsrLevelMgntSchema do begin
+//      UserList.Add(0,TUserWithLevelAccess.Create(0,'root','1','Main administrator',false, 1));
+//      UserList.Add(1,TUserWithLevelAccess.Create(1,'andi','2','A user',            false, 1));
+//      UserList.Add(2,TUserWithLevelAccess.Create(2,'user','3','Another user',      false, 10));
+//    end;
+    ASchema.Free;
+    AssertTrue('class function UsrMgntType not correct', (ASchema.UsrMgntType = TUsrMgntType.umtUnknown));
+
+  finally
+    ASchema.Free;
+  end;
+
+end;
+
+procedure TTestSecurityManagerSchema.CR_SchemaUserAuth;
+begin
+
+end;
+
 
 procedure TTestSecurityManagerSchema.SetUp;
 begin
